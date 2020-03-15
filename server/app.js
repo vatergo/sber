1f492f4c8bf42dc7cf3e0537506534802181d6d2
vatergo <vatergo@mail.ru>
2020-03-15 17:19:13 +0500

    heroku index

--- server/app.js
+++ server/app.js
@@ -12,6 +12,14 @@ const app = express();
 
 app.use(json());
 
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname + '/public/'));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/public/', 'index.html'));
+});
+
 app.use('/api/auth', auth);
 app.use('/api/categories', сategories);
 app.use('/api/products', products);
