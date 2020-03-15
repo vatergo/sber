53b9bbf0150a791c4b693102f7ba3381fecd51d4
vatergo <vatergo@mail.ru>
2020-03-15 17:02:41 +0500

    heroku client

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     historyApiFallback: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000'
+        target: 'mool-wary.herokuapp.com'
       },
     }
   },
