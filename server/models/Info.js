d6daaac85edb4aef7811c7ea7c2f801403c87a94
vatergo <vatergo@mail.ru>
2020-04-15 00:23:35 +0500

    required

--- server/models/Info.js
+++ server/models/Info.js
@@ -2,12 +2,12 @@ import { Schema, model } from 'mongoose';
 
 const schema = new Schema({
     studentId: { type: String, required: true },
-    photo: { type: String, required: true },
-    birthday: { type: Date, required: true },
-    address: { type: String, required: true },
-    education: { type: String, required: true },
-    info: { type: String, required: true },
-    diplom: { type: String, required: true }
+    photo: { type: String },
+    birthday: { type: String },
+    address: { type: String },
+    education: { type: String },
+    info: { type: String },
+    diplom: { type: String },
 });
 
 export default model('Info', schema);
